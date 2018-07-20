709a769762f693949db8554d9abeabb210536f3c
zhongmeicheng <feijinshuliu@163.com>
2018-07-20 16:35:13 +0800

    设计路由

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "hello-world"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//引入store
+import store from './store';
+
+Vue.config.productionTip = false;
+
+//eslint-disable no-new
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
