9ef07631bfcfbf4d893b99ea41e7a8c9002ac06a
zhongmeicheng <feijinshuliu@163.com>
2018-07-21 10:56:14 +0800

    解决bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Router = require('koa-router');
 //父路由
 const router = new Router();
 
-//bodyparser:该中间件用于post请求数据
+//bodyparser:该中间件用于post请求的数据
 const bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
 
@@ -19,7 +19,7 @@ const checkToken = require('./server/token/checkToken.js');
 
 //登录
 const loginRouter = new Router();
-loginRouter.post('/register', UserController.Login);
+loginRouter.post('/login', UserController.Login);
 //注册
 const registerRouter = new Router();
 registerRouter.post('/register', UserController.Reg);
@@ -31,11 +31,17 @@ userRouter.get('/user', checkToken, UserController.GetAllUsers);
 const delUserRouter = new Router();
 delUserRouter.post('/delUser', checkToken, UserController.DelUser);
 
+
+
+
 //装载上面四个子路由
 router.use('/api',loginRouter.routes(),loginRouter.allowedMethods());
 router.use('/api',registerRouter.routes(),registerRouter.allowedMethods());
 router.use('/api',userRouter.routes(),userRouter.allowedMethods());
 router.use('/api',delUserRouter.routes(),delUserRouter.allowedMethods());
+
+
+
 
 //加载路由中间件
 app.use(router.routes()).use(router.allowedMethods());
@@ -43,3 +49,4 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(8888, () => {
   console.log('The server is running at http://localhost:' + 8888);
 });
+
--- server/controller/user.js
+++ server/controller/user.js
@@ -4,22 +4,21 @@ const moment = require('moment');
 const objectIdToTimestamp = require('objectid-to-timestamp');
 //用于密码加密
 const sha1 = require('sha1');
-//创建token
+//createToken
 const createToken = require('../token/createToken.js');
 
-//数据库操作
-//根据用户名查用户
+//数据库的操作
+//根据用户名查找用户
 const findUser = (username) => {
   return new Promise((resolve, reject) => {
     User.findOne({ username }, (err, doc) => {
-      if(err) {
+      if (err) {
         reject(err);
       }
-      resolve(doc)
+      resolve(doc);
     });
   });
 };
-
 //找到所有用户
 const findAllUsers = () => {
   return new Promise((resolve, reject) => {
@@ -31,7 +30,6 @@ const findAllUsers = () => {
     });
   });
 };
-
 //删除某个用户
 const delUser = function(id) {
   return new Promise((resolve, reject) => {
@@ -47,26 +45,26 @@ const delUser = function(id) {
 
 //登录
 const Login = async(ctx) => {
-  //获取账号密码
+  //拿到账号和密码
   let username = ctx.request.body.name;
   let password = sha1(ctx.request.body.pass);
 
   let doc = await findUser(username);
 
-  if(!doc) {
+  if (!doc) {
     console.log('检查到用户名不存在');
     ctx.status = 200;
     ctx.body = {
       info: false
     }
-  } else if(doc.password === password) {
-    console.log('密码一致');
-    //生成一个新的的token，并保持到数据库
+  } else if (doc.password === password) {
+    console.log('密码一致!');
+    //生成一个新的token,并存到数据库
     let token = createToken(username);
     doc.token = token;
     await new Promise((resolve, reject) => {
       doc.save((err) => {
-        if(err) {
+        if (err) {
           reject(err);
         }
         resolve();
@@ -75,24 +73,32 @@ const Login = async(ctx) => {
 
     ctx.status = 200;
     ctx.body = {
+      success: true,
+      username,
+      token, //登录成功要创建一个新的token,应该存入数据库
+      create_time: doc.create_time
+    };
+  } else {
+    console.log('密码错误!');
+    ctx.status = 200;
+    ctx.body = {
       success: false
-    }
+    };
   }
-}
-
+};
 //注册
 const Reg = async(ctx) => {
   let user = new User({
     username: ctx.request.body.name,
-    password: sha1(ctx.request.body.pass),
-    token: createToken(this.username)
+    password: sha1(ctx.request.body.pass), //加密
+    token: createToken(this.username) //创建token并存入数据库
   });
 
-  //将objectid转换成用户创建时间
+  //将objectid转换为用户创建时间(可以不用)
   user.create_time = moment(objectIdToTimestamp(user._id)).format('YYYY-MM-DD HH:mm:ss');
-  let doc = await  findUser(user.username);
-  if(doc) {
-    console.log('用户已经存在');
+  let doc = await findUser(user.username);
+  if (doc) {
+    console.log('用户名已经存在');
     ctx.status = 200;
     ctx.body = {
       success: false
@@ -100,10 +106,12 @@ const Reg = async(ctx) => {
   } else {
     await new Promise((resolve, reject) => {
       user.save((err) => {
-        if(err) {
+        if (err) {
           reject(err);
         }
+
         resolve();
+
       });
     });
     console.log('注册成功');
@@ -113,17 +121,15 @@ const Reg = async(ctx) => {
     }
   }
 };
-
-//获取所有用户信息
+//获得所有用户信息
 const GetAllUsers = async(ctx) => {
-  //查询所有信息
+  //查询所有用户信息
   let doc = await findAllUsers();
   ctx.status = 200;
-
   ctx.body = {
-    success: '成功',
+    succsess: '成功',
     result: doc
-  }
+  };
 };
 
 //删除某个用户
@@ -143,5 +149,3 @@ module.exports = {
   GetAllUsers,
   DelUser
 };
-
-
